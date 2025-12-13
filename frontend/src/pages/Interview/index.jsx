7714f81321072baa987ac8afd2be576b96d7067f
Rishab-the-developer <rishabhkhanna500@gmail.com>
2025-12-13 12:09:35 +0530

    page folder deleted

--- frontend/src/pages/Interview/index.jsx
+++
@@ -1,9 +0,0 @@
-import AuthGuard from '../../components/AuthGuard'
-
-export default function Interview() {
-  return (
-    <AuthGuard>
-      <div>Interview</div>
-    </AuthGuard>
-  )
-}
--- frontend/src/pages/reports.jsx
+++
@@ -1,9 +0,0 @@
-import AuthGuard from '../components/AuthGuard'
-
-export default function Reports() {
-  return (
-    <AuthGuard>
-      <div>Reports</div>
-    </AuthGuard>
-  )
-}
