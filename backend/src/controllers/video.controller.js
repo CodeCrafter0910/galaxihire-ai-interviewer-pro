f9a0e5cc5c8e8ae678efe1a5473f449d8665ac69
Rishab-the-developer <rishabhkhanna500@gmail.com>
2026-02-04 20:11:10 +0530

    Add .js extension to Video require

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const Video = require("../models/Video");
+const Video = require("../models/Video.js");
 const logger = require("../logger");
 
 /**
